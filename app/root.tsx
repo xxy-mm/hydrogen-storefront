44e63aeaecf64dca40192a34e012360473003f67
darian <xxy.moming@gmail.com>
2024-03-15 17:25:10 +0800

    Generate routes for core functionality

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,6 @@ import favicon from './assets/favicon.svg';
 import resetStyles from './styles/reset.css';
 import appStyles from './styles/app.css';
 import {Layout} from '~/components/Layout';
-import tailwindCss from './styles/tailwind.css';
 
 /**
  * This is important to avoid re-fetching root queries on sub-navigations
@@ -46,7 +45,6 @@ export const shouldRevalidate: ShouldRevalidateFunction = ({
 
 export function links() {
   return [
-    {rel: 'stylesheet', href: tailwindCss},
     {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
     {
